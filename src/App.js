4fc7f29ee447172e98daba46059551213dbad361
muhammedjafer <mohammadjafer990@gmail.com>
2024-02-06 00:02:12 +0300

    File 24 of learning react.js

--- src/App.js
+++ src/App.js
@@ -3,26 +3,28 @@ import logo from "./logo.svg";
 import "./App.css";
 
 class App extends Component {
-
   constructor() {
     super();
 
     this.state = {
-      name: 'Muhammed jaafer Omer',
-      company: 'new company'
-    }
+      monsters: [
+        { name: "first", id: 1 },
+        { name: "second", id: 2 },
+        { name: "third", id: 3 },
+      ],
+    };
   }
 
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>Hello {this.state.name} i am going to a new {this.state.company}</p>
-          <button onClick={() => {this.setState({name: 'Andrei'})}}>
-            Get the current user
-          </button>
-        </header>
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
       </div>
     );
   }
