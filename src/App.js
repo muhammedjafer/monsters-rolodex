b869a81a14354b460bc2df84df6daf22c2656beb
muhammedjafer <mohammadjafer990@gmail.com>
2024-02-05 23:13:55 +0300

    file 20 of learning react

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ class App extends Component {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>Hello {this.state.name}</p>
+          <p>Hello {this.state.name} i am going to a new {this.state.company}</p>
           <button onClick={() => {this.setState({name: 'Andrei'})}}>
             Get the current user
           </button>
